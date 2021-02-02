c8d9e2cf9df2270211db5405b22f275cf94614fd
Victor Sanchez <incomingvrs@gmail.com>
2021-02-01 22:41:45 -0800

    Add results card directory, file and stylesheet

---
+++ frontendReact/sordaapp/src/components/ResultCard/ResultCard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './ResultCard.css';
+
+class ResultCard extends Component {
+    render() {
+        return(
+            <div className="card-container">
+                CARD HERE
+            </div>
+
+        );
+    }
+}

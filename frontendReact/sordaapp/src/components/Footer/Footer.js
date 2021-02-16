07adbe062f3b977d33af10fcb94a5d31d81378e8
Victor Sanchez <incomingvrs@gmail.com>
2021-02-16 09:14:56 -0800

    Finish initial hookup of Footer Component functionality

--- frontendReact/sordaapp/src/components/Footer/Footer.js
+++ frontendReact/sordaapp/src/components/Footer/Footer.js
@@ -6,8 +6,10 @@ class Footer extends Component {
         return(
             <footer id="app-footer">
                 {/*<form action="/action_page.php"> */}
-                    <button className="footer-button" type="submit" value="Save">Save</button>
-                    <button className="footer-button" type="submit" value="Shuffle">Shuffle</button>
+                    <button className="footer-button" type="submit" value="Save" onClick={() => console.log("SAVE BUTTON CLICKED...")}>Save</button> 
+                    
+                    <button className="footer-button" type="submit" value="Shuffle" onClick={() =>
+                    console.log("SHUFFLE BUTTON CLICKED...")}>Shuffle</button>
                 {/*</form>*/}
             </footer>
         )

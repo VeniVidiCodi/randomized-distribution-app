e943e97d46a1c2d60b8a9982f4ffd42f6455ef64
Victor Sanchez <incomingvrs@gmail.com>
2021-04-26 10:05:22 -0700

    Add 404 page component ‘NoMatch’ to Router, Add Switch tag wrapper to Routes, Create NoMatch component page and stylesheet to AppPages directory

--- frontendReact/sordaapp/src/App.js
+++ frontendReact/sordaapp/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 import LandingPage from './components/AppPages/LandingPage';
 import ResultsPage from './components/AppPages/ResultPage';
 import InputPage from './components/AppPages/InputPage';
+import NoMatch from './components/AppPages/NoMatch';
 
 
 
@@ -12,12 +13,14 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
-        <Route path='/' exact component={LandingPage}/>
-        <Route path='/grouper' exact component={InputPage}/>
-        <Route path='/results' exact component={ResultsPage}/>
-
-      </div>
+      <Switch>
+        <div className="App">
+          <Route path='/' exact component={LandingPage}/>
+          <Route path='/grouper' exact component={InputPage}/>
+          <Route path='/results' exact component={ResultsPage}/>
+          <Route component={NoMatch}/>
+        </div>
+      </Switch>
     </Router>
   );
 }
---
+++ frontendReact/sordaapp/src/components/AppPages/NoMatch.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../../mallard.svg';
+import './NoMatch.css';
+
+class NoMatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    //   idNumber: null
+    }
+  }
+
+  render(){
+    return (
+      <div class="wrapper">
+
+            {/* <div class="topSection">
+                <img src={logo} alt="duck logo" />
+            </div> */}
+            <div class="topSection">
+                <img src="https://media.giphy.com/media/xvGEx3cazysda/giphy.gif" alt="upside down duck in water" />
+            </div>
+
+            <div class="bottomSection">
+                <div class="buttonChild">
+                  <Link to="/">
+                    <button type="submit" value="Submit" id="btn-start">Home</button>
+                  </Link>
+                </div>
+            </div>
+
+      </div>
+    );
+  }
+}
+
+export default NoMatch;
